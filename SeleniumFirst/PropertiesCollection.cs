b4d42004418844246daae009156489949e53c728
Yurii <masterossnipe@gmail.com>
2018-07-13 15:02:47 +0300

    Changing names

--- SeleniumFirst/PropertiesCollection.cs
+++ SeleniumFirst/PropertiesCollection.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SeleniumFirst
+namespace AutomationTraining
 {
     enum PropertyType
     {
--- SeleniumFirst/SeleniumSetMethods.cs
+++ SeleniumFirst/SeleniumSetMethods.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Support.UI;
 
-namespace SeleniumFirst
+namespace AutomationTraining
 {
     public static class SeleniumSetMethods
     {
